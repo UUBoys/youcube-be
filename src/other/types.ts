92f57bfc1468a7166ef6fc93af16b0eee3aaf681
Daniel Seiner <51313652+dannyseiner@users.noreply.github.com>
2023-03-26 20:49:56 +0200

    types

--- src/other/types.ts
+++ src/other/types.ts
@@ -4,7 +4,41 @@ export type Immutable<T> = {
 
 export type JWTToken = {
   uuid: string;
-  
+
   iat?: number;
   exp?: number;
+};
+
+interface Video {
+  uuid: string;
+  user: User;
+  title: string;
+  monetized: boolean;
+  description: string;
+}
+
+interface User {
+  uuid: string;
+  verified: boolean;
+  name: string;
+  email: string;
+}
+
+interface Comment {
+  uuid: string;
+  parent_uuid: string;
+  user: User;
+  message: string;
+  created_at: string;
+}
+
+interface Tag {
+  id: number;
+  name: string;
+}
+
+interface PlayList {
+  uuid: string;
+  user: User;
+  videos: Video[];
 }
